520ff9359d8838d74986b2670b7df0cce1bf2c13
Liza <liza.chambi@gmail.com>
2020-02-12 14:52:42 -0300

    Menu de filtros

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import { Link, NavLink, Redirect } from 'react-router-i18n';
 import { generate } from 'shortid'
-import { Form, Nav, Navbar, NavDropdown, FormControl, Button} from 'react-bootstrap';
 import { connect } from 'react-redux'
 import { mapStateToProps, mapDispatchToProps} from '../../mapMethods'
 
 import './css/Footer.css'
 import I18n from '../I18n.js'
 
-const Footer = props => {
+const Footer = props => 
+{
   return (
     <div className="Footer">
       Viandas Ya - 2019-2020
       <div className= "Language">
-        <p><I18n t="footerLanguage" /></p>
+        <p><I18n t="footerLanguage"/></p>
         <ul>
-        <li key= {generate()}> <NavLink ignoreLocale to="/es/"> <I18n t="footerLanguageSpanish" /> </NavLink> </li>
+          <li key= {generate()}> <NavLink ignoreLocale to="/es/"> <I18n t="footerLanguageSpanish" /> </NavLink> </li>
           <li>-</li> 
-          <li key= {generate()}> <NavLink ignoreLocale to="/en/"> <I18n t="footerLanguageEnglish" /> </NavLink>
-          </li>
+          <li key= {generate()}> <NavLink ignoreLocale to="/en/"> <I18n t="footerLanguageEnglish" /> </NavLink> </li>
         </ul>
       </div>
     </div>
--- src/components/global/Search.js
+++ src/components/global/Search.js
@@ -1,27 +1,24 @@
 import React, {useState, useContext } from 'react' // <-- updated
+import { Card, Form, Nav, Navbar, NavDropdown, FormControl, Button, Container, Row, Col} from 'react-bootstrap';
+import { generate } from 'shortid';
 
 import I18n from '../I18n'
 import './css/Search.css'
-import { Form, Nav, Navbar, NavDropdown, FormControl, Button} from 'react-bootstrap';
 
 const Search = () => {
     
     return (
         <div className= 'Search'>
-            <h1> 
-                <I18n t="searchTitle" /> 
-            </h1>
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home"> <i>ViandaYa!</i> </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#link">Link</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Item> <I18n t="searchName" /> </NavDropdown.Item>
+                        <NavDropdown.Item> <I18n t="searchCategory" /> </NavDropdown.Item>
+                        <NavDropdown.Item> <I18n t="searchCity" /> </NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
@@ -32,8 +29,97 @@ const Search = () => {
                     </Form>
                 </Navbar.Collapse>
             </Navbar>
+
+            <div className= 'Filters'>
+                <Container fluid>
+                    <Row>
+                        <Col sm={2}>
+                            <div>
+                                <ul>
+                                    <p> <I18n t="searchFilter"/>: </p>
+                                    <li key= {generate()}> <I18n t="searchCategory"/>:
+                                    </li> 
+                                    <li> <I18n t="searchRank"/>: 
+                                        <li>
+                                        <I18n t="searchMax"/>: 
+                                        </li>
+                                        <li>
+                                        <I18n t="searchMin"/>: 
+                                        </li>
+                                    </li> 
+                                    <li key= {generate()}> <I18n t="searchPrice"/>: </li>
+                                </ul>
+                            </div> 
+
+                        </Col>
+                        <Col sm={10}> 
+                            <h3> <I18n t="searchTitle" /> </h3>
+                            <MenusParaVerComoQuedaLaBusqueda/>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
+
+const MenusParaVerComoQuedaLaBusqueda= () => {
+       const menusPrueba = {
+            menus: [
+            {
+                key: generate(),
+                name: 'Pizza Olivo',
+                description: 'Pizza de mozzarella cubierta de aceitunas.',
+                price: '120',
+                amountDaily: '200',
+                date1: '10/5/2019',
+                date2:'10/5/2020',
+            },
+            {
+                key: generate(),
+                name: 'Picante de pollo',
+                description: 'Pollo con salsa picante con guarnición de arroz.',
+                price: '150',
+                amountDaily: '150',
+                date1: '1/3/2019',
+                date2:'4/8/2020',
+            },
+            {
+                key: generate(),
+                name: 'Pique',
+                description: 'Salteado de verduras, huevo y carne asada.',
+                price: '150',
+                amountDaily: '150',
+                date1: '1/3/2019',
+                date2:'4/8/2020',
+            },
+            {
+                key: generate(),
+                name: 'Api morado',
+                description: 'Bebida de maiz morado con limón.',
+                price: '150',
+                amountDaily: '150',
+                date1: '1/3/2019',
+                date2:'4/8/2020',
+            }
+        ]}
+
+    return (
+        <div className= "CardsMenu">
+            { menusPrueba.menus.map(menu =>
+                    <Card className= "Card" style={{ width: '15rem'}}>
+                    <Card.Img variant="top" src={menu.image} />
+                    <Card.Body style= {{padding: '0.7rem'}}>
+                        <Card.Title>{menu.name}</Card.Title>
+                        <Card.Text> {menu.description}</Card.Text>
+                        <Button variant="primary">Ver menú</Button>
+                    </Card.Body>
+                    </Card>
+        )}
+        </div>
+  )
+}
+
+
 
 export default Search;
